ebf0ecbfb89953f2f0a821e71b89a91c3f593a72
sinsadongjin <135315817+sinsadongjin@users.noreply.github.com>
2024-02-24 22:40:32 +0900

    Delete windows directory

--- windows/activate.bat
+++
@@ -1,2 +0,0 @@
-FOR %%A IN (%~dp0\.) DO SET folder=%%~dpA
-call %folder%\.venv\Scripts\activate.bat
--- windows/start.bat
+++
@@ -1,4 +0,0 @@
-FOR %%A IN (%~dp0\.) DO SET folder=%%~dpA
-call %folder%\.venv\Scripts\activate.bat
-call python %folder%\run.py
-pause
